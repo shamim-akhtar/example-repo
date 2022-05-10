bc8ce2bf11513a0d6b304e506044b16419226b99
Shamim Akhtar <shamim.akhtar@gmail.com>
2022-05-10 16:23:46 +0800

    Implemented button clicks for all three buttons.

--- MyUnityProject/Assets/UIScript.cs
+++ MyUnityProject/Assets/UIScript.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class UIScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  // Start is called before the first frame update
+  void Start()
+  {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  public void OnButton1_Click()
+  {
+    Debug.Log("You have clicked Button1");
+  }
+
+  public void OnButton2_Click()
+  {
+    Debug.Log("You have clicked Button2");
+  }
+
+  public void OnButton3_Click()
+  {
+    Debug.Log("You have clicked Button3");
+  }
 }
